1f97a4e4e09c19ffefdcc6602da975b57768189d
haonanwu123 <92578731+haonanwu123@users.noreply.github.com>
2023-09-25 10:13:50 +0200

    update

--- app/App.js
+++ app/App.js
@@ -1,31 +1,17 @@
-import React from "react";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/home';
+import Settings from './screens/Settings';
 
-import { createAppContainer } from "react-navigation";
-import { createDrawerNavigator } from "react-navigation-drawer";
-import { Dimensions } from "react-native";
+const Drawer = createDrawerNavigator();
 
-import { Feather } from "@expo/vector-icons";
-
-import {
-  ProfileScreen,
-  MessageScreen,
-  ActivityScreen,
-  ListScreen,
-  ReportScreen,
-  StatisticScreen,
-  SignOutScreen
-} from "./screens";
-
-const DraweNavigator = createDrawerNavigator({
-  ProfileScreen,
-  MessageScreen,
-  ActivityScreen,
-  ListScreen,
-  ReportScreen,
-  StatisticScreen,
-  SignOutScreen
-});
-
-const Drawer = createDrawerNavigator()
-
-export default createAppContainer(DraweNavigator);
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Settings" component={Settings} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
